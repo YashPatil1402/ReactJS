fed0545ded1994a02e640e8fb6d1abc92356fbaf
Yash Patil <140960782+YashPatil1402@users.noreply.github.com>
2025-10-26 18:54:29 +0530

    update vite application

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Yash from "./Yash";
+
+function App() {
+  return (
+    <>
+    <div className="demo"><Yash />
+    <h1>Hello </h1></div>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/Yash.jsx
@@ -0,0 +1,7 @@
+function Yash() {
+    return (
+        <h2>Hello Yash!</h2>
+    );
+}
+
+export default Yash;
