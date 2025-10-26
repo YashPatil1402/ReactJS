510aa43bfe8d96b381bef01a1936514ae41f654c
Yash Patil <140960782+YashPatil1402@users.noreply.github.com>
2025-10-26 18:55:08 +0530

    update react application 01

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Yash from "./yash";
+
+function App() {
+  return (
+    <>
+    <Yash />
+    <h1>React Start!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/yash.jsx
@@ -0,0 +1,7 @@
+function Yash(){
+    return(
+        <h1>Hello Yash From React</h1>
+    )
+}
+
+export default Yash
